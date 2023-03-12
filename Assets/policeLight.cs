29ea2d440daacb8e1562a8e4476125f8e16dda19
RedZvER <6995335+RedZvER@users.noreply.github.com>
2023-03-12 14:05:24 +0500

    UI Update

--- Assets/policeLight.cs
+++ Assets/policeLight.cs
@@ -8,24 +8,27 @@ public class policeLight : MonoBehaviour
     [SerializeField] GameObject redLight = null;
     [SerializeField] GameObject blueLight = null;
     float curretTime = 0;
+    bool isRedFirst = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        curretTime = UnityEngine.Random.Range(0.00001f, timerPeriud);
-        redLight.SetActive(true);
-        blueLight.SetActive(false);
+        isRedFirst = Random.Range(0, 100) >= 50f;
+        curretTime = UnityEngine.Random.Range(0.00001f, timerPeriud * 10);
+        redLight.SetActive(isRedFirst);
+        blueLight.SetActive(!isRedFirst);
     }
 
     // Update is called once per frame
     void Update()
     {
         curretTime -= Time.deltaTime;
-        if (curretTime<0) {
+        if (curretTime < 0)
+        {
             redLight.SetActive(!redLight.activeSelf);
             blueLight.SetActive(!blueLight.activeSelf);
             curretTime = timerPeriud;
         }
-        
+
     }
 }
