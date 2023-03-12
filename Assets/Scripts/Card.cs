325f920d4a1beace7c007151aa584196ca20acae
RedZvER <6995335+RedZvER@users.noreply.github.com>
2023-03-12 15:14:55 +0500

    prepare merge

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -7,19 +7,26 @@ public class Card : MonoBehaviour
     [SerializeField] private Vector3 OnHoverPopup;
     [SerializeField] private float hoverAnimationTime = 0.5f;
     
+    private IEnumerator doHover;
+
     public void OnHoverEnter() {
-        
+        if(doHover != null) {
+            StopCoroutine(doHover);
+        }
+
+        doHover = DoHover(true);
     }
 
     public void OnHoverExit() {
 
     }
 
-    private IEnumerator DoHover() {
-        float hoverTimer = hoverAnimationTime;
+    private IEnumerator DoHover(bool show) {
+        float hoverTimer = (show) ? 0 : hoverAnimationTime;
 
-        while(hoverTimer > 0) {
-            hoverTimer -= Time.deltaTime;
+        while(hoverTimer > ((show) ? hoverAnimationTime : 0)) {
+            if(show) hoverTimer += Time.deltaTime; 
+            else hoverTimer -= Time.deltaTime;
 
             yield return new WaitForEndOfFrame();
         }
